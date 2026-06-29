f1a949a5b2098b6690cde4197e984d3c61ed6ada
RambergApps <alexbnielsen92@gmail.com>
2026-06-29 09:40:50 +0200

    Create data.js

---
+++ data/data.js
@@ -0,0 +1,26 @@
+window.NUSSEBASSENE_DATA = {
+  status: {
+    generated_at: null,
+    source: {
+      primary: "fifa_calendar",
+      control: "football-data.org"
+    },
+    matches: []
+  },
+  resultater: {
+    generated_at: null,
+    regler: {
+      regel: "90 minutter",
+      poeng_exact: 3,
+      poeng_outcome: 1
+    },
+    matches: [],
+    deltakere: {},
+    leaderboard: []
+  },
+  innleveringsstatus: {
+    generated_at: null,
+    rounds: {},
+    deltakere: []
+  }
+};
